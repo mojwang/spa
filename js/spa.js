f0f373221853bed047a6ec1e75323a2c2d35a962
Marvin Ojwang <mojwang@gmail.com>
2015-05-19 23:08:26 -0700

    Add spa shell, along with UI skeleton

---
+++ js/spa.js
@@ -0,0 +1,19 @@
+/*
+ * spa.js
+ * Root namespace module
+ */
+
+/* jshint
+   browser: true, continue: true, devel: true, indent: 4, maxerr: 50, 
+   newcap: true, nomen: true, plusplus: true, regexp: true, sloppy: true, 
+   vars: false, white: true
+ */ 
+/* global $, spa */
+
+var spa = (function () { 
+	var initModule = function ($container) { 
+		spa.shell.initModule($container);
+	};
+
+	return { initModule: initModule };
+}());
